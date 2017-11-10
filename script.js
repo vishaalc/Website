c63c57df27aa347665a274c7bec28dc9966278b9
Vishaal <vschittur@gmail.com>
2017-11-09 20:03:26 -0500

    V2.1
    rude boy ting ya big tings jeeeez
    - new design, mobile fix!

--- script.js
+++ script.js
@@ -1,80 +1,33 @@
-
 $(function(){
-	//Open freshaz
-  	$("#profile").addClass("loaded");
-  	var flags = {prev:"", open:false};
 
-	//==================== ICON ANIMATIONS ====================
-	$('.icon-container img').hover(
+	//$("#profile").animate({opacity:'1'}, 800);
+	$("#profile").animate({
+		width: "100%",
+		height: "100%",
+	}, 800 );
+
+	$("div.menu").animate({opacity:'1'}, 800);
+
+	var global = {open:false,currentpage:""};
+
+	$('.menu img').hover(
 		function() {
-	    	$(this).css({"cursor":"pointer", "left":"30px","padding-left":"-30px"});
-	    	$("#desc-container").css("display","block");
-	    	id = '#' + $(this).attr('data-id');
-	    	$(id).animate({ 
-	    		color: "#444444" 
-	    	}, 250);
+	    	$(this).css({"cursor":"pointer"});
+	    	$(this).parent("p").css({"padding-left":"20px"});
+	    	$(this).next().fadeIn("fast").css("display","table-cell");
 		}, 
 		function() {
-		    $(this).css({"cursor":"auto", "left":"0px","padding-left":"20px"});	
-		    $("#desc-container").css("display","none");
-		    id = '#' + $(this).attr('data-id');
-			$(id).animate({ 
-				color: "white" 
-			}, 250);
+		    $(this).css({"cursor":"auto"});
+	    	$(this).parent("p").css({"padding-left":"0px"});
+	    	$(this).next().fadeOut("fast").css("display","table-cell");;
 		}
 	);	
 
-	//==================== MENU BINDINGS ====================
-
-	/*
-	window.addEventListener('click', function(e){   
-  		if (document.getElementById('clickbox').contains(e.target)){
-   			// Clicked in box
-  		} else{
-   		 // Clicked outside the box
-  		}
-	});
-	*/
-
-	function draw(option){
-		$("#typed").hide();
-		if (!(flags.open)){
-			$("#profile").addClass("shrinker");
-			$("#profile").removeClass("loaded");
-			setTimeout(draw_top,0)
-			setTimeout(draw_right,500)
-			setTimeout(draw_bottom,1000)
-			setTimeout(draw_left,1500)
-			setTimeout(filler,2000)
-
-			flags.open = true;
-			flags.prev=$(option).attr('data-id');
-			pageHandler(flags.prev);
-		} else {
-			if (flags.prev == $(option).attr('data-id')){
-				$('#content_wrapper').stop().animate({
-         			backgroundColor: "transparent"
-        		}, 150 );
-				$("#profile").addClass("loaded");
-				
-				$("#top").animate({width:'0%'}, 800);
-				$("#right").animate({height:'0%'}, 800);
-				$("#bottom").animate({width:'0%'}, 800);
-				$("#left").animate({height:'0%'}, 800);
-
-				$("div.main").animate({opacity:'0'}, 800);
-
-				flags.open = false;
-
-				$("#typed").text('Hi, I\'m Vishaal').fadeIn("slow");
-
-			} else {
-				flags.prev = $(option).attr('data-id')
-				//build page
-				pageHandler($(option).attr('data-id'));
-
-			}
-		}
+	function draw(){
+		setTimeout(draw_top,0)
+		setTimeout(draw_right,500)
+		setTimeout(draw_bottom,1000)
+		setTimeout(draw_left,1500)
 	}
 
 	function draw_top(){
@@ -89,87 +42,103 @@ $(function(){
 	function draw_left(){
 		$("#left").animate({height:'100%'}, 500);
 	}
-	function filler(){
-		$('#content_wrapper').animate({
-          		backgroundColor: "#FCF6E3"
-        	}, 1000 );
-		$('div.main').animate({
-          		opacity:1
-        	}, 2000 );
-	}
 
-	
-	function pageHandler(page){
-		var html;
-		//$("#typed").hide();
-		switch (page) {
-			case ("about_me"):
-				$("#about_page").fadeIn().css("display","block");
-				$("#work_page").fadeOut().css("display","none");
-				$("#contact_page").fadeOut().css("display","none");
+	$("#navbar-cancel").on("click",function(){	
+		closePage();
+	})
 
-				$("#typed").text('About Me').fadeIn("slow");
-				break;
-			case ("my_work"):
-				$("#about_page").fadeOut().css("display","none");
-				$("#work_page").fadeIn().css("display","block");
-				$("#contact_page").fadeOut().css("display","none");
-
-				$("#typed").text('My Work').fadeIn("slow");
-				break;
-			case ("contact_more"):
-				$("#about_page").fadeOut().css("display","none");
-				$("#work_page").fadeOut().css("display","none");
-				$("#contact_page").fadeIn().css("display","block");
-
-				$("#typed").text('Contact +').fadeIn("slow");
-				break;
-		}
-		//return html;
-	}
-
-	$("div.navbar span.cancel").on("click",function(){
-		$('#content_wrapper').stop().animate({
-         	backgroundColor: "transparent"
-        }, 150 );
-		$("#profile").addClass("loaded");
+	function closePage(){
+		selector = "#" + global.currentpage
+		$(selector).removeClass("page-notransition");
+		$(selector).addClass("page-transition");
+		global.open = false;
+		global.currentpage = "";
 		$("#top").animate({width:'0%'}, 800);
 		$("#right").animate({height:'0%'}, 800);
 		$("#bottom").animate({width:'0%'}, 800);
 		$("#left").animate({height:'0%'}, 800);
-		$("div.main").animate({opacity:'0'}, 800);
 
-		flags.prev="";
-		flags.open=false;
-		$("#typed").hide();
-		$("#typed").text('Hi, I\'m Vishaal').fadeIn("slow");
-	})
-
-	$('.icon-container img').on("click", function(){
-		var section = $(this).attr('data-id')
+		$("#about_page").css("opacity",0);
+		$("#work_page").css("opacity",0);
+		$("#contact_page").css("opacity",0);
+	}
 		
+
+	$('.menu img').on("click", function(){
+		var section = $(this).attr('data-id');
+		if (!global.open) {
+			pageHandler(section);
+			draw();
+		} else {
+			if (global.currentpage==section){
+				closePage();
+			} else {
+				pageHandler(section);
+			}
+		}
+	});	
+
+	function pageHandler(section){
 		switch (section) {	
-			case "about_me":
-				console.log(1)
-				draw(this);
+			case "about_page":
+				$("#about_page").show();
+				if (!global.open){
+					$("#about_page").addClass("page-transition")
+					$("#about_page").removeClass("page-notransition")
+				} else {
+					$("#about_page").removeClass("page-transition")
+					$("#about_page").addClass("page-notransition")
+				}
+				$("#about_page").css("opacity",1);
+
+				$("#work_page").hide();
+				$("#work_page").css("opacity",0);
+
+				$("#contact_page").hide();
+				$("#contact_page").css("opacity",0);
+			
 				break;
-			case "my_work":
-				console.log(2)
-				draw(this);
+			case "work_page":
+				$("#work_page").show();
+				if (!global.open){
+					$("#work_page").addClass("page-transition")
+					$("#work_page").removeClass("page-notransition")
+				} else {
+					$("#work_page").removeClass("page-transition")
+					$("#work_page").addClass("page-notransition")
+				}
+				$("#work_page").css("opacity",1);
+
+				$("#about_page").hide();
+				$("#about_page").css("opacity",0);
+
+				$("#contact_page").hide();
+				$("#contact_page").css("opacity",0);
+			
+
 				break;
-			case "contact_more":
-				console.log(3)
-				draw(this);
+			case "contact_page":
+				$("#contact_page").show();
+				if (!global.open){
+					$("#contact_page").addClass("page-transition")
+					$("#contact_page").removeClass("page-notransition")
+				} else {
+					$("#contact_page").removeClass("page-transition")
+					$("#contact_page").addClass("page-notransition")
+				}
+				$("#contact_page").css("opacity",1);
+
+				$("#work_page").hide();
+				$("#work_page").css("opacity",0);
+
+				$("#about_page").hide();
+				$("#about_page").css("opacity",0);
 				break;
 			default:
 				console.log("to do");
 		}
-	});	
 
-	//==================== CANVAS ANIMATIONS ====================
-
-	
+		global.currentpage=section;
+		global.open=true;
+	}
 });
-
-	
-
