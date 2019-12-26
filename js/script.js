5432dc98cc6a110cd6d3ae6bdd3fbdbe283ea3b9
Vishaal Chittur <vschittur@gmail.com>
2019-12-26 14:53:49 -0500

    v3 rework
    * minimal
    
    * back to basics
    
    * styling done, adding content
    
    * basic styling done, added content
    
    * added icon
    
    * versioning

---
+++ js/script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+(function ($) {
+
+	$(document).ready(function () {
+
+		// Preload bounce
+		var $preload = $('#preload');
+		if ($preload.length) {
+			$(window).on('load', function () {
+				$preload.fadeOut(400);
+			});
+		}
+
+		// Toggle pages
+		$('li.menu-item').on('click', function (e) {
+		    var previousItem = $('.menu-list > li.active')
+		    previousItem.removeClass('active');
+		    $(this).addClass('active');
+
+		    var previousPage = previousItem.children('a').attr('target'),
+		    	nextPage = $(this).children('a').attr('target');
+		    $('#'+previousPage).hide(500);
+		    $('#'+nextPage).show(500);
+
+		    $header.removeClass('active');
+			$hideMenu.removeClass('active');
+		});
+
+		var $header = $('.header');
+
+		// Mobile Menu
+		var $menuBtn = $('.menu-mobile'),
+			$hideMenu = $('.hide-menu');
+
+		$menuBtn.on('click', function () {
+			$header.toggleClass('active');
+
+			if ($header.hasClass('active')) {
+				$hideMenu.addClass('active');
+			}
+			else {
+				$hideMenu.removeClass('active');
+			}
+		});
+
+		// Hide dim overlay
+		$hideMenu.on('click', function () {
+			$header.removeClass('active');
+			$hideMenu.removeClass('active');
+		});
+
+		
+	});
+
+})(jQuery);
